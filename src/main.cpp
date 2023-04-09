aafa5cd0e5c48be683a9db4bc4459b09eb5a6249
ronger <ronger-x@outlook.com>
2023-04-09 22:48:59 +0800

    :sparkles: 实现通过 EspTouch 配网, WiFi 配置存储到文件系统

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,25 @@
 #include "ArduinoJson.h"
 #include "PubSubClient.h"
 #include <WiFiClientSecure.h>
+#include <FS.h>
+#include <SPIFFS.h>
 
 #define RGB_PIO 38
+#define FORMAT_SPIFFS_IF_FAILED true
 static uint8_t s_led_state = 0;
-/* wifi config */
-const char *ssid = "ronger6";
-const char *password = "D724b28ff.";
 
-const char *host = "mqtt.rymcu.com";
-const int port = 8883;
-const char *topic = "rymcu";
-const char *mqttUser = "esp32s3";
-const char *mqttPassword = "esp32s3.";
+struct Config {
+    char ssid[32];
+    char password[32];
+    char mqttUser[32];
+    char mqttPassword[32];
+    char host[32];
+    int port;
+    char topic[32];
+};
 
-
+const char *filename = "/config.txt";
+Config config;
 // load DigiCert Global Root CA ca_cert
 const char *ca_cert = \
 "-----BEGIN CERTIFICATE-----\n" \
@@ -43,9 +48,58 @@ const char *ca_cert = \
 "-----END CERTIFICATE-----\n";
 
 WiFiClientSecure espClient;
+
 PubSubClient client(espClient);
 
+void writeFile(fs::FS &fs, const char *path, const char *message);
+
+void appendFile(fs::FS &fs, const char *path, const char *message);
+
+bool loadConfig();
+
 void setup_wifi() {
+    delay(10);
+    //Init WiFi as Station, start SmartConfig
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.beginSmartConfig();
+    //Wait for SmartConfig packet from mobile
+    Serial.println("Waiting for SmartConfig.");
+    while (!WiFi.smartConfigDone()) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("SmartConfig received.");
+
+    //Wait for WiFi to connect to AP
+    Serial.println("Waiting for WiFi");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    randomSeed(micros());
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    // 将 WiFi 配置信息写入文件
+    writeFile(SPIFFS, filename, "{\"ssid\":\"");
+    appendFile(SPIFFS, filename, WiFi.SSID().c_str());
+    appendFile(SPIFFS, filename, "\",\"password\":\"");
+    appendFile(SPIFFS, filename, WiFi.psk().c_str());
+    appendFile(SPIFFS, filename, "\",\n");
+    appendFile(SPIFFS, filename, "\"mqtt\": {\n");
+    appendFile(SPIFFS, filename, "\"host\": \"mqtt.rymcu.com\",\n");
+    appendFile(SPIFFS, filename, "\"port\": 8883,\n");
+    appendFile(SPIFFS, filename, "\"user\": \"esp32s3\",\n");
+    appendFile(SPIFFS, filename, "\"password\": \"esp32s3..\",\n");
+    appendFile(SPIFFS, filename, "\"topic\": \"rymcu\"\n");
+    appendFile(SPIFFS, filename, "}}\n");
+}
+
+void setup_wifi(const char *ssid, const char *password) {
     delay(10);
     // We start by connecting to a WiFi network
     Serial.println();
@@ -124,10 +178,10 @@ void reconnect() {
         // Create a random client ID
         String clientId = "ESP32-";
         clientId += String(random(0xffff), HEX);
-        if (client.connect(clientId.c_str(), mqttUser, mqttPassword)) {
+        if (client.connect(clientId.c_str(), config.mqttUser, config.mqttPassword)) {
             Serial.println("connected");
             client.publish("outTopic", ("hello world, i am " + clientId).c_str());
-            client.subscribe(topic);
+            client.subscribe(config.topic);
         } else {
             Serial.print("failed, rc=");
             Serial.print(client.state());
@@ -138,15 +192,109 @@ void reconnect() {
     }
 }
 
+void writeFile(fs::FS &fs, const char *path, const char *message) {
+    Serial.printf("Writing file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if (!file) {
+        Serial.println("- failed to open file for writing");
+        return;
+    }
+    if (file.print(message)) {
+        Serial.println("- file written");
+    } else {
+        Serial.println("- write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char *path, const char *message) {
+    Serial.printf("Appending to file: %s\r\n", path);
+    File file = fs.open(path, FILE_APPEND);
+    if (!file) {
+        Serial.println("- failed to open file for appending");
+        return;
+    }
+    if (file.print(message)) {
+        Serial.println("- message appended");
+    } else {
+        Serial.println("- append failed");
+    }
+    file.close();
+}
+
+bool loadConfig() {
+    Serial.printf("Reading file: %s\r\n", filename);
+    File configFile = SPIFFS.open(filename);
+    if (!configFile || configFile.isDirectory()) {
+        configFile.close();
+        Serial.println("Failed to open config file");
+        return false;
+    }
+
+    Serial.println("- read from file:");
+
+    size_t size = configFile.size();
+    if (size > 1024) {
+        Serial.println("Config file size is too large");
+        configFile.close();
+    }
+
+    std::unique_ptr<char[]> buf(new char[size]);
+
+    configFile.readBytes(buf.get(), size);
+    DynamicJsonDocument doc(size - 1);
+    DeserializationError error = deserializeJson(doc, buf.get(), size);
+
+    if (error) {
+        Serial.print("deserializeJson() failed: ");
+        Serial.println(error.c_str());
+        configFile.close();
+        return false;
+    }
+    strcpy(config.ssid, doc["ssid"]);
+    strcpy(config.password, doc["password"]);
+    strcpy(config.host, doc["mqtt"]["host"]);
+    config.port = doc["mqtt"]["port"];
+    strcpy(config.mqttUser, doc["mqtt"]["user"]);
+    strcpy(config.mqttPassword, doc["mqtt"]["password"]);
+    strcpy(config.topic, doc["mqtt"]["topic"]);
+    Serial.println(config.password);
+    Serial.println(config.topic);
+    configFile.close();
+    return true;
+}
+
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\r\n", path);
+    if(fs.remove(path)){
+        Serial.println("- file deleted");
+    } else {
+        Serial.println("- delete failed");
+    }
+}
+
 void setup() {
     Serial.begin(115200);
     while (!Serial) { delay(100); }
-
     // We start by connecting to a WiFi network
-    setup_wifi();
-    espClient.setCACert(ca_cert);
-    client.setServer(host, port);
-    client.setCallback(callback);
+    if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)) {
+        Serial.println("SPIFFS Mount Failed");
+        return;
+    } else {
+        Serial.println("SPIFFS Mount Success");
+        bool isConfig = loadConfig();
+        if (isConfig) {
+            Serial.println("Config loaded");
+            setup_wifi(config.ssid, config.password);
+        } else {
+            setup_wifi();
+            loadConfig();
+        }
+        espClient.setCACert(ca_cert);
+        client.setServer(config.host, config.port);
+        client.setCallback(callback);
+    }
 }
 
 void loop() {
